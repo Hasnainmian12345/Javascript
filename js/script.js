8a525b7337a6ccd2d925e8747e21493bfb233bb6
Hasnainmian12345 <Hasnainmian12345@gmail.com>
2025-10-08 11:57:37 +0500

    1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 
+function displayId(element){
+  console.log(element.id);
+}
 function closeMe(){x=document.getElementById("demo");x.className="closed";}
 
 function openMe(){x=document.getElementById("demo");x.className="open";}
