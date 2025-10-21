d39d30fa4cb54979215bb2ec50ead7ed11e14da9
Hasnainmian12345 <Hasnainmian12345@gmail.com>
2025-10-21 21:48:08 +0500

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,6 +1,6 @@
 /*Name this external file gallery.js*/
 
-function upDate(previewPic){document.getElementById('image').innerHTML='this.src';}
+function upDate(previewPic){document.getElementById('image').innerHTML='this.img';}
  /* In this function you should 
     1) change the url for the background image of the div with the id = "image" 
     to the source file of the preview image
