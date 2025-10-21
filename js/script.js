c6c6691490762e1a0dd5fda3a9b4ba476720465e
Hasnainmian12345 <Hasnainmian12345@gmail.com>
2025-10-21 16:18:57 +0500

    1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-function greet(name, age) {	return 'Hello ${name}! You are ${age} years old.';}				
+/*Name this external file gallery.js*/
 
-function showProperties(element){
-  document.getElementById('message').innerHTML = element.alt;
-}
-function displayId(element){
-  console.log(element.id);
-}
-function closeMe(){x=document.getElementById("demo");x.className="closed";}
+function upDate(previewPic){
+ /* In this function you should 
+    1) change the url for the background image of the div with the id = "image" 
+    to the source file of the preview image
+    
+    2) Change the text  of the div with the id = "image" 
+    to the alt text of the preview image 
+    */
+  
+	}
 
-function openMe(){x=document.getElementById("demo");x.className="open";}
+	function unDo(){
+     /* In this function you should 
+    1) Update the url for the background image of the div with the id = "image" 
+    back to the orginal-image.  You can use the css code to see what that original URL was
+    
+    2) Change the text  of the div with the id = "image" 
+    back to the original text.  You can use the html code to see what that original text was
+    */
+		
+	}
