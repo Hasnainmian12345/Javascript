bf990777a51c8bf6905ff3781eebb1c1915d5eb6
Hasnainmian12345 <Hasnainmian12345@gmail.com>
2025-10-22 15:21:01 +0500

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,7 @@
 /*Name this external file gallery.js*/
 
 
-function upDate(previewPic){document.getElementById('image').style.backgroundImage=url('"+previewPic.src+"');document.getElementById('image').innerHTML = element.alt;}
+function upDate(previewPic){document.getElementById('image').style.backgroundImage=url('"+previewPic.src+"');document.getElementById('image').innerHTML = 'element.alt';}
  
 
 /* In this function you should 
