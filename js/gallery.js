029baeeff93c03c9c274273edec11550af86fd8e
Hasnainmian12345 <Hasnainmian12345@gmail.com>
2025-10-25 22:22:57 +0500

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -10,17 +10,12 @@ function upDate(previewPic){document.getElementById('image').style.backgroundIma
     to the alt text of the preview image 
     */
 function setImages(){options=["fort_in_woods.png","istockphoto-1419410282-612x612.jpg","pexels-pixabay-158063.jpg","colourful.jpg","high_altitude_aerial_view_above_cotton_soft_cloud_tops_glowing_in_golden_hour_light-full.jpg","pexels-alena-koval-233944-886521.jpg"]
-
-    currentImages = document.querySelectorAll(".flex img");
-
-  for (let i = 0; i < currentImages.length; i++) {
-    console.log("Image " + i);
-    randomImg = options[Math.floor(Math.random() * options.length)];
-    imagePath = "images/" + randomImg.src;
-
-    currentImages[i].src = imagePath;
-    currentImages[i].alt = randomImg.alt; 
-    currentImages[i].setAttribute("tabindex", "0");}}
+currentImages=document.querySelectorAll(".flex img")
+for(var i=0; i<currentImages.length; i++)
+{console.log("Image"+i);
+randomImg="images/"+options[Math.floor(Math.random()*options.length)];
+currentImages[i].src=randomImg;currentImages[i].alt =options[Math.floor(Math.random() * options.length)].alt;currentImages[i].setAttribute("tabindex","0");
+;}}
 
 
 
